37f1ea019d9e2dc07e57bfc0bd915c7b0abae348
Paul Byrne <code@pfaocle.co.uk>
2014-12-08 21:58:00 +0000

    Add a minimal Codeception 2 set-up (with no tests).

---
+++ tests/tests/_support/FunctionalHelper.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Codeception\Module;
+
+/**
+ * Define custom actions: all public methods declared in helper class will be available in $I
+ */
+class FunctionalHelper extends \Codeception\Module
+{
+}
