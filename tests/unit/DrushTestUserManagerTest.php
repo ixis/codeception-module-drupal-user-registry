b4652dbcae0be33589e7227b8ab20a6bd4aeee0f
Paul Byrne <code@pfaocle.co.uk>
2014-12-14 16:32:41 +0000

    Add test for DrushTestUserManager::message()

--- tests/unit/DrushTestUserManagerTest.php
+++ tests/unit/DrushTestUserManagerTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use \Codeception\Lib\Console\Message;
 use \Codeception\Util\Fixtures;
 use \Codeception\Module\Drupal\UserRegistry\DrushTestUserManager;
 
@@ -53,5 +54,58 @@ class DrushTestUserManagerTest extends \Codeception\TestCase\Test
             "Please configure the drush-alias setting in your suite configuration."
         );
         new DrushTestUserManager(Fixtures::get("invalidModuleConfig"));
+    }
+
+    /**
+     * Test message().
+     *
+     * @test
+     * @group protected
+     */
+    public function testMessage()
+    {
+        // Test we receive the expected object with both a message and no message (empty string).
+        foreach (["", "This is the message to output."] as $message) {
+            $this->messageWithString($message);
+        }
+    }
+
+    /**
+     * Helper function for testMessage()
+     *
+     * @param string $message
+     *   The message to test with.
+     */
+    protected function messageWithString($message)
+    {
+        // Set up.
+        $output = new Codeception\Lib\Console\Output(array());
+        $refMethod = self::getProtectedMethod("message");
+        $testUserManager = new DrushTestUserManager(Fixtures::get("validModuleConfig"));
+
+        $expected = new Codeception\Lib\Console\Message($message, $output);
+        $actual = $refMethod->invokeArgs($testUserManager, array($message));
+        $this->assertInstanceOf('\Codeception\Lib\Console\Message', $actual);
+
+        // Note we're only comparing the string-converted object as the $stream member variable is different between
+        // instances.
+        $this->assertEquals($expected->__toString(), $actual->__toString());
+        $this->assertEquals($message, $actual->__toString());
+    }
+
+    /**
+     * Get a protected method of a class via ReflectionClass.
+     *
+     * @param string $name
+     *   The name of the protected method.
+     *
+     * @return ReflectionMethod
+     */
+    protected static function getProtectedMethod($name)
+    {
+        $class = new ReflectionClass('\Codeception\Module\Drupal\UserRegistry\DrushTestUserManager');
+        $method = $class->getMethod($name);
+        $method->setAccessible(true);
+        return $method;
     }
 }
