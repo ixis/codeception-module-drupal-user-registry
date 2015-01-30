6714b273f09e511c05af0dac1647255e5faef50f
Paul Byrne <code@pfaocle.co.uk>
2015-01-30 21:55:10 +0000

    Simplify fixtures a bit.

--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -20,44 +20,16 @@ $mockValidModuleConfig = array(
 );
 
 // Define a configuration which is invalid solely because the drush-alias entry is missing.
-$mockInvalidModuleConfig = array(
-    "roles" => array("administrator", 'editor', 'moderator', 'Authenticated'),
-    "password" => "test123!",
-    "create" => false,
-    "delete" => false,
-    "root" => array(
-        "username" => "root",
-        "password" => "root",
-    ),
-);
+$mockInvalidModuleConfig = $mockValidModuleConfig;
+unset($mockInvalidModuleConfig["drush-alias"]);
 
 // Define a complete, valid configuration with a custom username prefix.
-$mockCustomPrefixModuleConfig = array(
-    "roles" => array("administrator", 'editor', 'moderator', 'Authenticated'),
-    "password" => "test123!",
-    "create" => false,
-    "delete" => false,
-    "drush-alias" => "@d7.local",
-    "root" => array(
-        "username" => "root",
-        "password" => "root",
-    ),
-    "username-prefix" => "custom",
-);
+$mockCustomPrefixModuleConfig = $mockValidModuleConfig;
+$mockCustomPrefixModuleConfig["username-prefix"] = "custom";
 
 // Define a complete configuration with an invalid custom username prefix.
-$mockInvalidCustomPrefixModuleConfig = array(
-    "roles" => array("administrator", 'editor', 'moderator', 'Authenticated'),
-    "password" => "test123!",
-    "create" => false,
-    "delete" => false,
-    "drush-alias" => "@d7.local",
-    "root" => array(
-        "username" => "root",
-        "password" => "root",
-    ),
-    "username-prefix" => "xyz",
-);
+$mockInvalidCustomPrefixModuleConfig = $mockValidModuleConfig;
+$mockInvalidCustomPrefixModuleConfig["username-prefix"] = "xyz";
 
 Fixtures::add("validModuleConfig", $mockValidModuleConfig);
 Fixtures::add("invalidModuleConfig", $mockInvalidModuleConfig);
