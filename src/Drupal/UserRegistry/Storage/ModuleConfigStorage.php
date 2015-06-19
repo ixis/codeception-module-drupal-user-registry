f38d9aaae4bfdeebe96c462a64d3d70dd25bcb71
Chris Cohen <C.Cohen@ucas.ac.uk>
2015-06-19 16:45:08 +0100

    Fixed a bug where an array was a level deeper than expected in the emails config property.

--- src/Drupal/UserRegistry/Storage/ModuleConfigStorage.php
+++ src/Drupal/UserRegistry/Storage/ModuleConfigStorage.php
@@ -52,7 +52,7 @@ class ModuleConfigStorage implements StorageInterface
     public function __construct($config)
     {
         $this->roles = $config['roles'];
-        $this->emails = $config['emails'];
+        $this->emails = $config['emails'][0];
         $this->password = $config['password'];
     }
 
