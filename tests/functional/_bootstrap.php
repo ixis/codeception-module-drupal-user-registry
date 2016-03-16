fa561e600495397099ad729c948026a4453139e6
Andy Rigby <andy.rigby@ixis.co.uk>
2016-03-16 16:34:27 +0000

    Revert accidental alias change for functional tests

--- tests/functional/_bootstrap.php
+++ tests/functional/_bootstrap.php
@@ -29,7 +29,7 @@ $mockValidModuleConfig = array(
             "roles" => array("moderator"),
         ),
     ),
-    "drush-alias" => "@dr7.vagrant.remote",
+    "drush-alias" => "@d7.local",
 );
 Fixtures::add("validModuleConfig", $mockValidModuleConfig);
 
@@ -45,6 +45,6 @@ $mockValidModuleConfigWithRoot = array(
             "root" => true,
         ),
     ),
-    "drush-alias" => "@dr7.vagrant.remote",
+    "drush-alias" => "@d7.local",
 );
 Fixtures::add("validModuleConfigWithRoot", $mockValidModuleConfigWithRoot);
